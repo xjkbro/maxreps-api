697e0c6f97fa818b083d6f8fd75b0a05fb23e1e3
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-11-11 23:37:49 -0800

    ??

--- config/session.php
+++ config/session.php
@@ -155,8 +155,8 @@ return [
     |
     */
 
-    // 'domain' => env('SESSION_DOMAIN'),
-    'domain' => '.jsondelara.com',
+    'domain' => env('SESSION_DOMAIN'),
+    // 'domain' => '.jsondelara.com',
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+Route::get('/{any}', function () {
+    return view('app');
+    })->where('any', '.*');
 
 require __DIR__.'/auth.php';
