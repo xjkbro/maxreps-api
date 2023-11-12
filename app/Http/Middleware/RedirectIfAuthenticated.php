b2ebbf774eb28619f26e699f6ca0f8427af2c30d
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-11-11 23:06:42 -0800

    ?

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,8 +22,8 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 if ($request->expectsJson()) {
-                    return response()->json(['error' => 'Already authenticated.'], 200);
-                  }
+                    return response()->json(['error' => 'Authenticated.'], 200);
+                }
                 return redirect(RouteServiceProvider::HOME);
             }
         }
